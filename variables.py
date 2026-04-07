da9cc235ec4c07a1e56e0fff3c72f68ea9f3d8eb
Javier0422 <javier.morales56@inacapmail.cl>
2026-04-07 10:22:31 -0400

    AP_N1_C2

--- variables.py
+++ variables.py
@@ -4,12 +4,20 @@
 #print(saludo + nombre)
 
 # Ingrese 2 números mediante su teclado y muestre el resultado de la suma 
-print('Sumar dos números enteros')
-número_1 = input("Ingrese el primer número ")
-número_2 = input("Ingrese el segundo número ")
-suma = int(número_1) + int(número_2)
-print()
+#print('Sumar dos números enteros')
+#número_2 = input("Ingrese el segundo número ")
+#suma = int(número_1) + int(número_2)
+#print()
 #print("La suma de los números es: " + str(suma))
 
-print(f"La suma de los números es: {suma}")
-print(f'{número_1} + {número_2} = {suma}')
+#print(f"La suma de los números es: {suma}")
+#print(f'{número_1} + {número_2} = {suma}')
+
+# Conviertiendo los textos en numeros 
+numero_1 = int(input('Imgrese el primer numero entero: '))
+numero_2 = int(input('Ingrese el segundo numero entero: '))
+
+# Despues convertir a numero, viene la operacion aritmetica
+resultado = numero_1 + numero_2
+
+# 
