c4d5ef225c449688128a0b62366ebdcfcd59fb0b
Javier0422 <javiermoralesdietz53@gmail.com>
2026-04-08 10:10:06 -0400

    inputs

---
+++ inputs.py
@@ -0,0 +1,7 @@
+# Cada datp ingreasdo mediante um input Es un Texto
+
+numero = input('Por favor, ingrese un número entero: ')
+print(type(numero))
+print(numero)
+
+print(f'Sú NUMERO multiplicado por 2 es: {int(numero) * 2}')
