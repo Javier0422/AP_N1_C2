bc4b3cb7e4776a84e5cf34e1aeea5904814933e2
Javier0422 <javiermoralesdietz53@gmail.com>
2026-06-19 23:31:45 -0400

    si

---
+++ modulos/datos/data_libros.py
@@ -0,0 +1,13 @@
+from negocio.negocio_libros import procesar_libro
+from datos import listado_libros
+import os
+
+def escribir_data_libros():
+    lista_modificada = procesar_libro()
+
+    ruta_archivo = os.path.join('modulos/datos','info_libros.py')
+    ruta_absoluta = os.path.abspath(ruta_archivo)
+    ruta_final = os.path.realpath(ruta_absoluta)
+    archivo_libros = open(ruta_final,'+w', encoding="utf-8")
+    archivo_libros.write(f'listado_libros = {lista_modificada}')
+    archivo_libros.close()
